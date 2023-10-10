3f6fda8aadcaf800ecd4d614274c9e06a6ac293e
Pranav Likhi <83015339+plikhi14756@users.noreply.github.com>
2023-10-10 04:34:41 -0400

    Added the files HTML and CSS basic files
    These files has information about all the basic concepts which are important for web development

---
+++ web development basic concepts.js
@@ -0,0 +1,52 @@
+
+// introduction to web development theoratical concepts
+
+// 1. the internet basics - internet is a type of network in which 2 computers connect with each other and share information.  
+
+// 2. basic working of internet - when user enter the intenet with the help of computer the to access internet. 
+
+// 3. web page - it is a special type of document which can be accessed only on internet and it is created by using HTML language.
+
+// 4. website - it the collection of web pages connected together and can be accesed through single domain is known as website.
+
+// 5. web server - it the a special type of computer/server for internet which has the collection of various websites or webpages and their required files is known as web server. this webserver allows the user on the internet to access the web page or website or its resources as per request if the user knows the address of the website/web page 
+
+// 6. search engine - its a special type of web service which allows user to type the website address and access it through webserver on internet is known as search engine 
+
+// 7. URL - it stands for (Unique resource locator)
+
+
+// ** all things web page/website , webserver and search engine are interconnected with each other so that that the user can access website or required webpages
+
+
+
+// A website is usually made with the combination of HTML, CSS and Javascript each language plays a critical role in the creation of the website 
+
+// This how the combination all these languages helps in the creation of a professional website
+
+// HTML (Hyper text markup language) - helps in creating basic structure of the website 
+// CSS - helps in adding style to the website
+// Javascript - helps in adding dynamic functionality to the website
+// typescript - helps in adding static/syntax typing to Javascript
+
+// files and conditions which are important part of the website
+
+// every website must have 4 files
+
+// 1. index.html - this file will store the html code and text content which will provide a basic structure to our website
+
+// 2. media file - this file will store the media content like images, video or audios and many other media relatedc things of the website
+
+// 3. CSS file - this file will store the css language code that will provide style to our website
+
+// 4. Scripting file - This file wil have the javascript code which add some dynamic functionality to tyhe website 
+
+// important conditions for the the website to be developed and work sucessful 
+
+// 1. All the files must be stored together under a single folder 
+
+// 2. space should not be used to seperate file name because some webserver replace space with %20 charactor which may lead the created site link to be broken or some server treat space in a way that it seperate a single file name into two diferent filenames 
+
+// 3. While providing name to the website only hyphen (-) symbol should be used because all webservers will treat this as word seperator and this will ensure that website will run properly and it will not lead to a broken link
+
+// 4. While running or testing a website all these files must be connected to each other by using some HTML code
